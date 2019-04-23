8396144f98570a7c6dd00fd132c7282ff6ddda6a
Artem Zimin <atm.zmn95@gmail.com>
2019-04-23 21:40:01 +0300

    paginated fetch (p.101)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'Redux';
+const DEFAULT_HPP = 100;
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
 
@@ -27,11 +28,24 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+
+    const oldHits = page !== 0
+      ? this.state.result.hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({ 
+      result: { hits: updatedHits, page }
+    });
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page=0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
@@ -62,14 +76,14 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
-
-    if (!result) {
-      return null;
-    }
+    const page = (result && result.page) || 0;
 
     return (
       <div className="page">
         <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
@@ -78,18 +92,20 @@ class App extends Component {
             Search
           </Search>
         </div>
-        { result &&
-          <Table 
-            list={result.hits}
-            onDismiss={this.onDismiss} />
-        }
+        <div className="content">
+          { result &&
+            <Table 
+              list={result.hits}
+              onDismiss={this.onDismiss} />
+          }
+        </div>
       </div>
     );
   }
 }
 
 const Search = ({ value, onChange, onSubmit, children }) => (  
-  <form onSubmit={onSubmit}>
+  <form onSubmit={onSubmit} className="search">
     <input
       type="text"
       value={value}
