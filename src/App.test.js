49405f09bddc2ef7facfb59e10618cff7b2d55ed
Artem Zimin <atm.zmn95@gmail.com>
2019-04-29 22:05:51 +0300

    unit tests with Enzyme (p.150)

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App, { Search, Button, Table } from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 // "describe" defines test suite
 // "it" describes one test case
@@ -56,6 +60,16 @@ describe('Button', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('onClick prop is called when clicked', () => {
+    const mockOnClick = jest.fn();
+    const element = shallow(
+      <Button onClick={mockOnClick} />
+    );
+
+    element.simulate('click');
+    expect(mockOnClick.mock.calls.length).toBe(1);
+  })
 });
 
 describe('Table', () => {
@@ -80,4 +94,12 @@ describe('Table', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('shows two items in list', () => {
+    // renders the component without its children
+    const element = shallow(
+      <Table {...props} />
+    );
+    expect(element.find('.table-row').length).toBe(2);
+  })
 });
